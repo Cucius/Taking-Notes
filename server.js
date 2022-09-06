bdadaea3f8ed765345569139f6113fbd32ce0890
Cucius <Rylan19@Gmail.com>
2022-09-05 20:39:57 -0600

    Reformated to * for index.html

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static("public"));
 
 // GET Route for homepage
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
 //GET Route for notes
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
 
