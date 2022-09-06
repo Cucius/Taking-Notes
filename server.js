43a93a5ed0f760b00290a3f77b8f6ede1f727c7d
Cucius <Rylan19@Gmail.com>
2022-09-05 20:44:18 -0600

    Added code to create a base page

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // GET Route for homepage
 
-// app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
 
 //GET Route for notes
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
