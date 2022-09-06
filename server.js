a9b5c3b22879cd8cd8d629bbc9e3fd7682abd4d3
Cucius <Rylan19@Gmail.com>
2022-09-05 18:14:31 -0600

    Added code to join public with dirname

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // GET Route for homepage
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
