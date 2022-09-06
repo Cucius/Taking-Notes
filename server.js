88f644816743dbb2bf00a1b8c11c52ed8a07bc86
Cucius <Rylan19@Gmail.com>
2022-09-05 20:26:52 -0600

    Reverting code due to error

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // GET Route for homepage
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
 //GET Route for notes
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
 
