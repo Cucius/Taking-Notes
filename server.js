042277879a736e6af45344ff4713c39068fa7f6f
Cucius <Rylan19@Gmail.com>
2022-09-05 19:43:53 -0600

    Reformated to try and fix bug

--- server.js
+++ server.js
@@ -24,14 +24,25 @@ app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/note
 app.get("/api/notes", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
-//POST
-app.post("/api/notes", (req, res) => {
-  const addedNotes = req.body;
 
-  readFromFile("./db/db.json")
-    .then((data) => res.json(JSON.parse(data)))
-    .then(data.push(addedNotes))
-    .then((data[data.length - 1].id = data.length - 1));
-  writeToFile("./db/db.json").then((data) => res.json(JSON.stringify(data)));
+app.post("/api/notes", function (req, res) {
+  const addedNotes = req.body;
+  readFromFile("./db/db.json").then(function (data) {
+    data = JSON.parse(data);
+    data.push(addedNotes);
+    data[data.length - 1].id = data.length - 1;
+    writeToFile("./db/db.json", JSON.stringify(data));
+  });
 });
+
+//POST
+// app.post("/api/notes", (req, res) => {
+//   const addedNotes = req.body;
+
+//   readFromFile("./db/db.json")
+//     .then((data) => res.json(JSON.parse(data)))
+//     .then(data.push(addedNotes))
+//     .then((data[data.length - 1].id = data.length - 1));
+//   writeToFile("./db/db.json").then((data) => res.json(JSON.stringify(data)));
+// });
 app.listen(PORT, () => console.log(`Listen at http://localhost:${PORT}`));
